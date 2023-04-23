3e54843ebc6d6dd71842ce80b814841542382e4c
SzegBer <szegedi.bernadett@gmail.com>
2023-04-23 17:41:51 +0200

    swiper half way done

--- scripts/script.js
+++ scripts/script.js
@@ -197,6 +197,21 @@ const technologies = `
 
 
 
+const swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
 
 
 // --------------- SELECTORS ------------------------------
@@ -274,8 +289,8 @@ const jobsPageLoader = () => {
 
 
 
-
+/*
 mainPageLoader();
-
+*/
 
 
